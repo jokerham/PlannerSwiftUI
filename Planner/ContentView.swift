642aa333c5d05e7c6715945e3ea5bd55af66939a
Dongkyun Ham <jokerham@gmail.com>
2021-02-20 13:28:10 +0900

    Initial Commit

---
+++ Planner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Planner
+//
+//  Created by 함동균 on 2021/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
